c00a66440649e87dea83b866ad1629e077dc70a9
Davit Khaburdzania <davit.khaburdzania@gmail.com>
2014-04-21 00:06:17 +0400

    added some gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,35 @@
+var path = require('path');
 var gulp = require('gulp');
-
-gulp.task('default', function() {
-  console.log('test')
+var browserify = require('browserify');
+var less = require('gulp-less');
+var forever = require('forever-monitor');
+var source = require('vinyl-source-stream');
+ 
+gulp.task('browserify', function() {
+  return browserify('./public/scripts/app.js')
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('./public/dist/'));
 });
+
+gulp.task('less', function() {
+  gulp.src('./public/styles/bundle.less')
+    .pipe(less({ compress: true }))
+    .pipe(gulp.dest('./public/dist'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./public/scripts/*.js', ['browserify']);
+  gulp.watch('./public/styles/*.less', ['less']);
+});
+
+// gulp.task('forever', function () {
+//   forever.stop('./app.js');
+//   forever.start('./app.js');
+// });
+
+// gulp.task('server', function () {
+//   gulp.watch('./**/**/**/*.js', 'forever')
+// });
+
+gulp.task('default', ['browserify', 'less', 'watch']);
