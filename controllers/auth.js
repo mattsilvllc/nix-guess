026726d0937c075dc0fab42c0da7ccc0684ca704
Davit Khaburdzania <davit.khaburdzania@gmail.com>
2014-04-22 22:31:46 +0400

    create or update user after authentication

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,16 @@
 var passport = require('passport')
 var Fb_strategy = require('passport-facebook').Strategy;
+var User = Models.User;
 
 passport.serializeUser(function(user, done) {
-  done(null, user);
+  done(null, user.id);
 });
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
+passport.deserializeUser(function(id, done) {
+  User.find_by_id(id)
+    .then(function (user) {
+      done(null, user);
+    })
+    .fail(done);
 });
 
 passport.use(new Fb_strategy({
@@ -13,12 +18,35 @@ passport.use(new Fb_strategy({
     clientSecret: config.get('FACEBOOK_APP_SECRET'),
     callbackURL: config.get('APP_URL') + '/auth/facebook/callback'
   },
-  function(accessToken, refreshToken, profile, done) {
-    done(null, profile);
+  function(access_token, refresh_token, profile, done) {
+    profile = profile._json;
+    var user = {
+      fb_id: profile.id,
+      fb_token: access_token,
+      email: profile.email,
+      first_name: profile.first_name,
+      last_name: profile.last_name,
+      gender: profile.gender,
+      birthday: new Date(profile.birthday),
+      last_login: new Date()
+    };
+
+    User.find_by_fb_id(user.fb_id)
+      .then(function (exists) {
+        var data = [user.fb_token, new Date()];
+        var promise = exists ? User.update(exists.id, data) : User.create(user);
+        return promise;
+      })
+      .then(function (new_user) {
+        done(null, new_user);
+      })
+      .fail(done)
   }
 ));
 
-app.get('/auth/facebook', passport.authenticate('facebook'));
+app.get('/auth/facebook', passport.authenticate('facebook', {
+  scope: ['email', 'user_birthday']
+}));
 
 app.get('/auth/facebook/callback', passport.authenticate('facebook', {
   successRedirect: '/guess',
--- models/user.js
+++ models/user.js
@@ -1,9 +1,54 @@
 module.exports = {
-  save: function (user) {
+  create: function (user) {
+    var deffered = Q.defer();
+    var sql = 'insert into guess_user set ?';
+    var self = this;
 
+    user.created_at = new Date();
+
+    DB.query(sql, user, function (error, result) {
+      if(error) return deffered.reject(error);
+      self.find_by_id(result.insertId).then(deffered.resolve, deffered.reject);
+    });
+
+    return deffered.promise;
   },
-  
-  exists: function (email) {
 
+  update: function (id, data) {
+    var deffered = Q.defer();
+    var sql = 'update guess_user set fb_token = ?, last_login = ? where id = ?';
+    var self = this;
+
+    data.push(id);
+    DB.query(sql, data, function (error) {
+      if(error) return deffered.reject(error);
+      self.find_by_id(id).then(deffered.resolve, deffered.reject);
+    });
+
+    return deffered.promise;
+  },
+
+  find_by_id: function (id) {
+    var deffered = Q.defer();
+    var sql = 'select * from guess_user where id=?';
+
+    DB.query(sql, [id], function (error, result) {
+      if(error) return deffered.reject(error);
+      deffered.resolve(result[0]);
+    });
+
+    return deffered.promise;
+  },
+
+  find_by_fb_id: function (fb_id) {
+    var deffered = Q.defer();
+    var sql = 'select * from guess_user where fb_id=' + fb_id;
+
+    DB.query(sql, function (error, result) {
+      if(error) return deffered.reject(error);
+      deffered.resolve(result[0]);
+    });
+
+    return deffered.promise;
   }
 };
