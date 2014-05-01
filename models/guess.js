c7e3ede40dd42277a3a7dd7be648618c88f87eb9
Davit Khaburdzania <davit.khaburdzania@gmail.com>
2014-05-01 11:44:59 +0400

    altered random guess query, fixes #7

--- models/guess.js
+++ models/guess.js
@@ -3,7 +3,7 @@ module.exports = {
     var deffered = Q.defer();
     var sql = 'SELECT * FROM `nutritionix-api`.items_with_package_photos as products ' +
         'where products.upc NOT IN(select upc from `nutritionix-lab`.`guess_user_response`' +
-        ' where user_id=?) ORDER BY RAND() LIMIT 1'
+        ' where user_id=?) AND nf_serving_size_qty=1 AND nf_calories > 30 ORDER BY RAND() LIMIT 1'
     
     DB.query(sql, [user.id], function (error, result) {
       error ? deffered.reject(error) : deffered.resolve(result[0]);
